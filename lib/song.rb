43e748a74d4c01f8c8b2cf0ca5dd19802473bf56
Catherine Price <catprice444@gmail.com>
2020-05-19 23:26:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,20 +28,22 @@ end
 
 def self.genre_count
   genre_count = {}
-  if genre_count[@genre]
-    genre_count[@genre] += 1
+  @@genre.each do |genre|
+  if genre_count[genre]
+    genre_count[genre] += 1
   else
-    genre_count[@genre] = 1
+    genre_count[genre] = 1
   end
+end
   genre_count
 end
 
 def self.artist_count
   artist_count = {}
-  if artist_count[@artist]
-    artist_count[@artist] += 1
+  if artist_count[artist]
+    artist_count[artist] += 1
   else
-    artist_count[@artist] = 1
+    artist_count[artist] = 1
   end
   artist_count
 end
