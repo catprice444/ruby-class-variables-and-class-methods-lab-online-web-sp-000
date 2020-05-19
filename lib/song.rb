b9cc733aa311167ae5a1b2a5680a97eee7468d3e
Catherine Price <catprice444@gmail.com>
2020-05-19 23:11:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,14 @@ attr_accessor :name, :artist, :genre
 
 @@count = 0
 @@artists
-@@genres = []
+# @@genres = []
 
 def initialize (name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
   @@count +=1
-  @@genre << genre
+  # @@genre << genre
 end
 
 def count
